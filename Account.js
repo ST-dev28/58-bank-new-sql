d32021a050cdb030032eed00448dce19e961a8a3
SandraT <sndrbltr@gmail.com>
2021-09-29 16:38:43 +0000

    Account transactions, User all balance and delete

--- Account.js
+++ Account.js
@@ -7,7 +7,7 @@ const Account = {};
  * Saskaitos irasymas i duombaze.
  * @param {Object} connection Objektas, su kuriuo kvieciame duombazes mainpuliavimo metodus.
  * @param {number} userId Vartotojo ID.
- * @returns {Promise<object>} Saskaitos objektas.
+ * @returns {Promise<object|Error>} Saskaitos objektas.
  */
 Account.create = async (connection, userId) => {
     if (!Valid.id(userId)) {
@@ -22,8 +22,102 @@ Account.create = async (connection, userId) => {
     try {
         const [response] = await connection.execute(query);
         return {
+            operation: 'account_create',
             id: response.insertId,
             accountNumber,
+        }
+    } catch (error) {
+        return error;
+    }
+}
+
+/**
+ * Pinigu kiekis vartotojo nurodytoje saskaitoje.
+ * @param {Object} connection Objektas, su kuriuo kvieciame duombazes mainpuliavimo metodus.
+ * @param {string} accountNumber Vartotojo banko saskaitos numeris.
+ * @returns {Promise<object|boolean|Error>} Pinigu balanso objektas.
+ */
+Account.balance = async (connection, accountNumber) => {
+    if (!Valid.accountNumber(accountNumber)) {
+        return false;
+    }
+
+    const query = 'SELECT `money`\
+                    FROM `accounts`\
+                    WHERE `account_number` = "' + accountNumber + '"\
+                        AND `isActive` = 1';
+    try {
+        const [rows] = await connection.execute(query);
+
+        if (rows.length === 1) {
+            return {
+                operation: 'balance',
+                money: rows[0].money
+            };
+        } else {
+            return false;
+        }
+    } catch (error) {
+        return error;
+    }
+}
+
+/**
+ * Vartotojo saskaitos aktyvumo statusas.
+ * @param {Object} connection Objektas, su kuriuo kvieciame duombazes mainpuliavimo metodus.
+ * @param {string} accountNumber Vartotojo banko saskaitos numeris.
+ * @returns {Promise<object|boolean|Error>} Saskaitos aktyvumo objektas.
+ */
+Account.isActive = async (connection, accountNumber) => {
+    if (!Valid.accountNumber(accountNumber)) {
+        return false;
+    }
+
+    const query = 'SELECT `isActive`\
+                    FROM `accounts`\
+                    WHERE `account_number` = "' + accountNumber + '"';
+    try {
+        const [rows] = await connection.execute(query);
+
+        if (rows.length === 1) {
+            return rows[0].isActive === 1 ? true : false;
+        } else {
+            return false;
+        }
+    } catch (error) {
+        return error;
+    }
+}
+
+/**
+ * Vartotojo saskaitos uzdarymas.
+ * @param {Object} connection Objektas, su kuriuo kvieciame duombazes mainpuliavimo metodus.
+ * @param {string} accountNumber Vartotojo banko saskaitos numeris.
+ * @returns {Promise<object|boolean|Error>} Uzdarytos saskaitos objektas.
+ */
+Account.delete = async (connection, accountNumber) => {
+    if (!Valid.accountNumber(accountNumber)) {
+        return false;
+    }
+
+    const currentBalance = await Account.balance(connection, accountNumber);
+    if (!Valid.money(currentBalance.money) ||
+        currentBalance.money !== 0) {
+        return false;
+    }
+
+    const query = 'UPDATE `accounts`\
+                    SET `isActive` = 0\
+                    WHERE `account_number` = "' + accountNumber + '"';
+    try {
+        const [response] = await connection.execute(query);
+        if (response.affectedRows === 1 && response.changedRows === 1) {
+            return {
+                operation: 'account_delete',
+                accountNumber,
+            }
+        } else {
+            return false;
         }
     } catch (error) {
         return error;
@@ -35,7 +129,7 @@ Account.create = async (connection, userId) => {
  * @param {Object} connection Objektas, su kuriuo kvieciame duombazes mainpuliavimo metodus.
  * @param {string} accountNumber Vartotojo banko saskaitos numeris.
  * @param {number} cashAmount Inesamos pinigu sumos kiekis (centais).
- * @returns {*}
+ * @returns {Promise<object|boolean|Error>} Pinigu inesimo objektas.
  */
 Account.deposit = async (connection, accountNumber, cashAmount) => {
     if (!Valid.accountNumber(accountNumber) ||
@@ -45,13 +139,100 @@ Account.deposit = async (connection, accountNumber, cashAmount) => {
 
     const query = 'UPDATE `accounts`\
                     SET `money` = `money` + '+ cashAmount + '\
-                    WHERE `account_number` = "' + accountNumber + '"';
+                    WHERE `account_number` = "' + accountNumber + '"\
+                        AND `isActive` = 1';
     try {
         const [response] = await connection.execute(query);
         if (response.affectedRows === 1 && response.changedRows === 1) {
             return {
+                operation: 'deposit',
                 cashAmount,
                 accountNumber,
+            }
+        } else {
+            return false;
+        }
+    } catch (error) {
+        return error;
+    }
+}
+
+/**
+ * Pinigu isigryninimas is vartotojo nurodytos saskaitos.
+ * @param {Object} connection Objektas, su kuriuo kvieciame duombazes mainpuliavimo metodus.
+ * @param {string} accountNumber Vartotojo banko saskaitos numeris.
+ * @param {number} cashAmount Isimamos pinigu sumos kiekis (centais).
+ * @returns {Promise<object|boolean|Error>} Pinigu isnesimo objektas.
+ */
+Account.withdraw = async (connection, accountNumber, cashAmount) => {
+    if (!Valid.accountNumber(accountNumber) ||
+        !Valid.money(cashAmount)) {
+        return false;
+    }
+
+    const currentBalance = await Account.balance(connection, accountNumber);
+    if (!Valid.money(currentBalance.money) ||
+        currentBalance.money < cashAmount) {
+        return false;
+    }
+
+    const query = 'UPDATE `accounts`\
+                    SET `money` = `money` - '+ cashAmount + '\
+                    WHERE `account_number` = "' + accountNumber + '"\
+                        AND `isActive` = 1';
+    try {
+        const [response] = await connection.execute(query);
+
+        if (response.affectedRows === 1 && response.changedRows === 1) {
+            return {
+                operation: 'withdraw',
+                cashAmount,
+                accountNumber,
+            }
+        } else {
+            return false;
+        }
+    } catch (error) {
+        return error;
+    }
+}
+
+/**
+ * Pinigu pervedimas is vieno vartotojo nurodytos saskaitos i kito vartotojo nurodyta saskaita.
+ * @param {Object} connection Objektas, su kuriuo kvieciame duombazes mainpuliavimo metodus.
+ * @param {string} senderAccountNumber Siunciancio vartotojo banko saskaitos numeris.
+ * @param {string} receiverAccountNumber Gaunancio vartotojo banko saskaitos numeris.
+ * @param {number} cashAmount Pervedamas pinigu sumos kiekis (centais).
+ * @returns {Promise<object|boolean|Error>} Pinigu pervedimo objektas.
+ */
+Account.transfer = async (connection, senderAccountNumber, receiverAccountNumber, cashAmount) => {
+    if (!Valid.accountNumber(senderAccountNumber) ||
+        !Valid.accountNumber(receiverAccountNumber) ||
+        !Valid.money(cashAmount)) {
+        return false;
+    }
+
+    const receiverAccountIsActive = await Account.isActive(connection, receiverAccountNumber);
+    if (!receiverAccountIsActive) {
+        return false;
+    }
+
+    const currentBalance = await Account.balance(connection, senderAccountNumber);
+    if (!Valid.money(currentBalance.money) ||
+        currentBalance.money < cashAmount) {
+        return false;
+    }
+
+    try {
+        const withdrawResponse = await Account.withdraw(connection, senderAccountNumber, cashAmount);
+        const depositResponse = await Account.deposit(connection, receiverAccountNumber, cashAmount);
+
+        if (withdrawResponse !== false && depositResponse !== false) {
+            return {
+                operation: 'transfer',
+                cashAmount,
+                sender: senderAccountNumber,
+                receiver: receiverAccountNumber,
             }
         } else {
             return false;
--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.init = async () => {
     const conn = await db.init({
         host: 'localhost',
         user: 'root',
-        database: 'newbank',
+        database: 'bank',
     });
 
     // LOGIC BELOW
@@ -22,6 +22,9 @@ app.init = async () => {
     console.log(maryte);
     console.log(jonas);
     console.log(sonata);
+
+    const petrasBalance1 = await Account.balance(conn, petras.accountNumber);
+    console.log(petrasBalance1);
 
     const petroSaskaita1 = await Account.create(conn, petras.id);
     const sonatosSaskaita2 = await Account.create(conn, sonata.id);
@@ -36,6 +39,26 @@ app.init = async () => {
     console.log(petroDeposit1);
     console.log(sonatosDeposit1);
     console.log(sonatosDeposit2);
+
+    const sonatosWithdraw1 = await Account.withdraw(conn, sonata.accountNumber, 500);
+    const sonatosWithdraw2 = await Account.withdraw(conn, sonata.accountNumber, 1500);
+
+    console.log(sonatosWithdraw1);
+    console.log(sonatosWithdraw2);
+
+    const sonatosTransfer1 = await Account.transfer(conn, sonata.accountNumber, maryte.accountNumber, 200);
+
+    console.log(sonatosTransfer1);
+
+    const jonoDelete = await User.delete(conn, jonas.id);
+    const sonatosDelete = await User.delete(conn, sonata.id);
+
+    console.log(jonoDelete);
+    console.log(sonatosDelete);
+
+    const sonatosTransfer2 = await Account.transfer(conn, sonata.accountNumber, jonas.accountNumber, 50);
+
+    console.log(sonatosTransfer2);
 }
 
 app.init();
